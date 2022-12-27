51a9727e9cce3ee20794d6024776b5c58a212a45
Joshua Muhammad <joshua.muhammad928@gmail.com>
2022-12-26 23:30:04 -0800

    wip: pass data from front to back

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static('.'));
+app.use(express.static("."));
 app.get("/", (req, res) => {
   res.send("hello world!");
+});
+app.post("/", (req, res) => {
+  console.log("IM IN!");
 });
 app.listen(port, () => {
   console.log(`example app listening on port ${port}`);
