6d9fd41afd697650d5c55fceadff9ffdd242559d
Joshua Muhammad <joshua.muhammad928@gmail.com>
2022-12-28 20:57:52 -0800

    wip: pass data to the backend

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+let req= {body:{message: "shit"}};
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("."));
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
 app.post("/", (req, res) => {
-  console.log("IM IN!");
+  console.log(req.body);
 });
 app.listen(port, () => {
   console.log(`example app listening on port ${port}`);
