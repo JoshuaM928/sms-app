f4eac24b00cedf5ce9d8ee7ad382b654f9409510
Joshua Muhammad <joshua.muhammad928@gmail.com>
2022-12-28 20:57:52 -0800

    feat: pass data to the backend

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("."));
 app.get("/", (req, res) => {
-  res.send("hello world!");
+  res.sendStatus(200);
 });
 app.post("/", (req, res) => {
-  console.log("IM IN!");
+  console.log(req.body);
 });
 app.listen(port, () => {
   console.log(`example app listening on port ${port}`);
