f2cae5221fac8cd0cc4877c86fe47f578aa205e2
Joshua Muhammad <joshua.muhammad928@gmail.com>
2023-01-08 06:27:43 -0800

    wip: send data to the backend

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
+/**
+ * Express setup
+ */
 const express = require("express");
 const app = express();
 const port = 3000;
+app.use(express.static("public/images/"));
+app.use(express.static("routes/"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(express.static('.'));
+/**
+ * Routes
+ */
 app.get("/", (req, res) => {
-  res.send("hello world!");
+  res.sendFile("index.html");
 });
+app.get("/conversation", (req, res, next) => {
+  req.accepts("text/html");
+  res.send("you've made it to the http://127.0.0.1:3000/conversation url");
+});
+app.post("/conversation", (req, res, next) => {
+  console.log(req.body);
+  res.send("The backend received your message!");
+});
+app.get("/favicon.ico", (req, res, next) => {
+  res.sendFile(__dirname + "/favicon.ico");
+});
+
 app.listen(port, () => {
-  console.log(`example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
-app.get;
