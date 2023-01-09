5ed7097131304aae619a94517894e8967e343c72
Joshua Muhammad <joshua.muhammad928@gmail.com>
2023-01-09 15:49:09 -0800

    wip: moving to laptop

--- app.js
+++ app.js
@@ -6,24 +6,21 @@ const app = express();
 const port = 3000;
 app.use(express.static("public/images/"));
 app.use(express.static("routes/"));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 /**
  * Routes
  */
-app.get("/", (req, res) => {
-  res.sendFile("index.html");
-});
-app.get("/conversation", (req, res, next) => {
-  req.accepts("text/html");
-  res.send("you've made it to the http://127.0.0.1:3000/conversation url");
-});
 app.post("/conversation", (req, res, next) => {
-  console.log("HIT")
+  const data = req.body;
+  console.log(data.message);
+  res.json({ confirm: "confirmation" });
   // console.log(req.body);
-  res.send("The backend received your message!");
 });
+
+/**
+ * Necessary express routes
+ */
 app.get("/favicon.ico", (req, res, next) => {
   res.sendFile(__dirname + "/favicon.ico");
 });
