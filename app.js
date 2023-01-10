1901e8c579a29a32781ab1c9826b24e7ab15e84a
Joshua Muhammad <joshua.muhammad928@gmail.com>
2023-01-09 21:04:03 -0800

    build: front end message to backend to twilio api

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 /**
+ * Twilio setup
+ */
+const twilio = require("twilio");
+const client = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
+/**
  * Express setup
  */
 const express = require("express");
@@ -9,7 +18,28 @@ app.use(express.static("routes/"));
 app.use(express.json());
 
 /**
- * Routes
+ * Twilio routes
+ */
+app.post("/twilio/send", (req, res, next) => {
+  const data = req.body;
+  client.messages.create({
+    body: data.message,
+    to: data.number,
+    from: "+17262684011",
+  });
+  console.log("-----------------------");
+  console.log(`Incoming Message: ${data.message}`);
+  console.log("-----------------------");
+  res.send({ confirm: "confirmation" });
+  next();
+});
+app.get("/twilio/receive", (req, res, next) => {
+  res.send("URI: /twilio/receive");
+  console.log("URI: /twilio/receive");
+});
+
+/**
+ * Express routes
  */
 app.post("/conversation", (req, res, next) => {
   const data = req.body;
